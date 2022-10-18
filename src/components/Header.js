e13ca891ccfba50266ed2fec6a80560260880037
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-10-17 20:40:21 -0700

    add picture

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
+import friendsLogo from "./../img/Friends_logo.svg.png";
 
 function Header() {
-  return <h1>"Friends" web store</h1>;
+  return (
+    <React.Fragment>
+      <img src={friendsLogo} alt="Logo" width={1100} />
+      <h1> Web store</h1>;
+    </React.Fragment>
+  );
 }
 
 export default Header;
